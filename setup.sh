5576fabd4f24b13d4e8ed4b9d9ea0b8b04aae295
Andy Waite <13400+andyw8@users.noreply.github.com>
2022-12-13 14:01:28 -0500

    Spin setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+# See also:
+# - https://github.com/Shopify/dotfiles-starter-template
+# - https://vault.shopify.io/page/Isospin-tools~dhb1fb6.md
+
 echo "Running setup.sh for spin"
+
+THOUGHTBOT_DOTFILES_PATH="~/src/github.com/thoughtbot/dotfiles"
+
+if [ ! -d "$THOUGHTBOT_DOTFILES_PATH" ]; then
+  echo "Cloning thoughtbot/dotfiles"
+  mkdir -p "$THOUGHTBOT_DOTFILES_PATH"
+  git clone https://github.com/thoughtbot/dotfiles "$THOUGHTBOT_DOTFILES_PATH"
+fi
+
+ln -sf ~/src/github.com/thoughtbot/dotfiles/aliases ~/.aliases
+
+source ~/.aliases
