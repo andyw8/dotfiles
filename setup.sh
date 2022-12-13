210a23f74aa1e45e044d13b4d295014cae5a03a4
Andy Waite <13400+andyw8@users.noreply.github.com>
2022-12-13 14:02:25 -0500

    Shallow clone

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ THOUGHTBOT_DOTFILES_PATH="~/src/github.com/thoughtbot/dotfiles"
 if [ ! -d "$THOUGHTBOT_DOTFILES_PATH" ]; then
   echo "Cloning thoughtbot/dotfiles"
   mkdir -p "$THOUGHTBOT_DOTFILES_PATH"
-  git clone https://github.com/thoughtbot/dotfiles "$THOUGHTBOT_DOTFILES_PATH"
+  git clone --depth 1 https://github.com/thoughtbot/dotfiles "$THOUGHTBOT_DOTFILES_PATH"
 fi
 
 ln -sf ~/src/github.com/thoughtbot/dotfiles/aliases ~/.aliases
