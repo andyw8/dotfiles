ea2841ddc7331e89aa1342889ac6ab83a68bc43b
Andy Waite <andyw8@users.noreply.github.com>
2024-05-26 18:28:12 -0400

    Update fzf config

--- fzf.zsh
+++ fzf.zsh
@@ -6,8 +6,8 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/andy/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/andy/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
