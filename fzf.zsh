8c35dd3cdc8ee4f0385ec892a908076acd7d2d0d
Andy Waite <andyw8@users.noreply.github.com>
2025-11-15 13:28:28 -0500

    Simplify fzf config

--- fzf.zsh
+++ fzf.zsh
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == */Users/andy/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/Users/andy/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$HOME/.fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
